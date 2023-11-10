fa9ccce505741547f087aedc41f3490e1d6183ea
Isabelbenke <149741219+Isabelbenke@users.noreply.github.com>
2023-11-10 05:16:44 -0500

    Update TestCase1_Driver.java

--- src/TestCase1_Driver.java
+++ src/TestCase1_Driver.java
@@ -4,7 +4,6 @@ public class TestCase1_Driver
 {
     public static void main(String[] args)
     {
-        UncontrolledCSTR cstr = new UncontrolledCSTR();
 
         DecimalFormat df_t = new DecimalFormat("0.00");
         DecimalFormat df_C = new DecimalFormat("0.#####");
@@ -14,64 +13,17 @@ public class TestCase1_Driver
         double v_0 = 0.05; //m^3/min
         double V = 1; //m^3
 
-        double[] reactionConditions = {k_A,C_A0};
-        double[] opConditions = {v_0/V};
-
-        double C_Ai = 0.;
-        double C_Bi = 0.;
         double t = 0.;
-        double h = 0.01;
-        int i=0;
-        double epsilon;
+        double C_A = C_A0;
+        double C_B = 0.;
 
-        double[] C_A_Array = new double[i];
-        double[] C_B_Array = new double[i];
+        double[] reactionConditions = {k_A,C_A0};
+        double[] opConditions = {v_0};
 
-        double C_Atest = cstr.calculateExitConcentration(t,C_Ai,h,opConditions,reactionConditions,1);
+        UncontrolledCSTR cstr = new UncontrolledCSTR(1, opConditions, reactionConditions);
 
-        System.out.println(C_Atest+"\n");
+        cstr.runCSTR(100, 1);
 
-        System.out.println("x\t\t\ty_A\t\t\t\ty_B");
-
-        do
-        {
-            double[] tmpC_A_Array = new double[i+1];
-            double[] tmpC_B_Array = new double[i+1];
-
-            for(int j=0;j<i;j++)
-            {
-                tmpC_A_Array[j] = C_A_Array[j];
-            }
-
-            tmpC_A_Array[i] = cstr.calculateExitConcentration(t,C_Ai,h,opConditions,reactionConditions,1);
-            C_A_Array = tmpC_A_Array;
-
-            for(int j=0;j<i;j++)
-            {
-                tmpC_B_Array[j] = C_B_Array[j];
-            }
-
-            tmpC_B_Array[i] = cstr.calculateExitConcentration(t,C_Bi,h,opConditions,reactionConditions,2);
-            C_B_Array = tmpC_B_Array;
-
-            System.out.println(df_t.format(t)+"\t\t"+df_C.format(C_A_Array[i])+"\t\t"+df_C.format(C_B_Array[i]));
-
-            if(i<3)
-            {
-                epsilon = 1;
-            }
-            else
-            {
-                epsilon = 100*Math.abs(C_B_Array[i]-C_B_Array[i-1])/Math.abs(C_B_Array[i-1]);
-            }
-
-            C_Ai = C_A_Array[i];
-            C_Bi = C_B_Array[i];
-            t+=h;
-            i++;
-        }
-        while(epsilon>0.01);
-
-        System.out.println("\n"+df_t.format(t)+"\t\t"+df_C.format(C_Ai)+"\t\t"+df_C.format(C_Bi));
+        System.out.println(cstr.toString());
     }
 }
