1d38f79cedf1c6a256a2af8bd9c9769f0d173ed9
Isabelbenke <149741219+Isabelbenke@users.noreply.github.com>
2023-11-10 05:23:46 -0500

    Update TestCase1_Driver.java

--- src/TestCase1_Driver.java
+++ src/TestCase1_Driver.java
@@ -11,11 +11,6 @@ public class TestCase1_Driver
         double C_A0 = 0.2; //mol/m^3
         double k_A = 0.2; //min^-1
         double v_0 = 0.05; //m^3/min
-        double V = 1; //m^3
-
-        double t = 0.;
-        double C_A = C_A0;
-        double C_B = 0.;
 
         double[] reactionConditions = {k_A,C_A0};
         double[] opConditions = {v_0};
