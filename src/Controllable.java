16431cbe6460204a2b2b54fe9cc1d31173ee9266
sophiamckillop <149805155+sophiamckillop@users.noreply.github.com>
2023-11-23 12:06:58 -0500

    Update Controllable.java

--- src/Controllable.java
+++ src/Controllable.java
@@ -1,7 +1,7 @@
 public interface Controllable
 {
-    double setManipulatedVariable(double manipulatedVariable);
-    double readControlledVariable(double controlledVariable);
+    double setManipulatedVariable(double manipulatedVariable); //for changing flowrate
+    double readControlledVariable(double controlledVariable); //for reading concentration
     //double getProcessVariable();
     //void calculateControl();
 }
